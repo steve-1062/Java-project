6579c95d1994e62dd7062d06b854e49c03837e56
steve-1062 <96217211+steve-1062@users.noreply.github.com>
2022-04-01 12:37:10 +0530

    Add files via upload

---
+++ project.java
@@ -0,0 +1,38 @@
+
+import pack1.*;
+
+public class project
+{
+    
+    public static void main(String[] args)
+    {
+       
+        //demo obj1 = new demo();
+        //obj1.show();
+        //vehicle obj2 = new vehicle();
+        //obj2.addVehicle();
+        //obj2.vehicledelivery();
+        
+        //emp e1 = new emp();
+        //e1.addEmployee();
+        //e1.calculatesalary();
+        //e1.calculatesalary(basicsalary);
+
+        
+        
+       // customer c1 = new customer();
+        //c1.addCustomer();
+
+       // payment p1 = new payment();
+        //p1.addPayment();
+        //p1.calculatePayment();
+
+        //booking b1 = new booking();
+        //b1.bookingamt(100000,107000);
+
+   
+        dealer d1 = new dealer();
+        d1.dealertarget();
+    }
+}
+
